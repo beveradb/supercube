ba5cc05c053d10a214f31abd6038af370a3701eb
Andrew Beveridge <andrew@beveridge.uk>
2018-11-19 01:31:20 +0000

    Added save on tap, plus new route to saved suggestions list view

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,118 @@ class SupercubeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Supercube',
-      home: SupercubeHome(title: 'Supercube'),
+      home: SupercubeHome(),
     );
   }
 }
 
 class SupercubeHome extends StatefulWidget {
-  SupercubeHome({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-  final wordPair = WordPair.random();
-
   @override
-  _SupercubeHomeState createState() => _SupercubeHomeState();
+  SupercubeHomeState createState() => new SupercubeHomeState();
 }
 
-class _SupercubeHomeState extends State<SupercubeHome> {
+class SupercubeHomeState extends State<SupercubeHome> {
   @override
+  final String appBarTitle = "Supercube";
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    Widget _buildRow(WordPair pair) {
+      final bool alreadySaved = _saved.contains(pair);
+
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: new Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        },
+      );
+    }
+
+    Widget _buildSuggestions() {
+      return ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          // The itemBuilder callback is called once per suggested word pairing,
+          // and places each suggestion into a ListTile row.
+          // For even rows, the function adds a ListTile row for the word pairing.
+          // For odd rows, the function adds a Divider widget to visually
+          // separate the entries. Note that the divider may be difficult
+          // to see on smaller devices.
+          itemBuilder: (context, i) {
+            // Add a one-pixel-high divider widget before each row in theListView.
+            if (i.isOdd) return Divider();
+
+            // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+            // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+            // This calculates the actual number of word pairings in the ListView,
+            // minus the divider widgets.
+            final index = i ~/ 2;
+            // If you've reached the end of the available word pairings...
+            if (index >= _suggestions.length) {
+              // ...then generate 10 more and add them to the suggestions list.
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(widget.wordPair.asPascalCase),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(appBarTitle),
+          actions: <Widget>[
+            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          ],
+        ),
+        body: _buildSuggestions());
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
       ),
     );
   }
